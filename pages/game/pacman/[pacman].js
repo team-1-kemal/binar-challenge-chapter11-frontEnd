6a9e2ea5bd95e6b5c762a66d2e215b052064f3c3
fureya48 <fathur.rohman2535@gmail.com>
2022-11-27 17:54:06 +0700

    fix fetching api game

--- pages/game/pacman/[pacman].js
+++ pages/game/pacman/[pacman].js
@@ -19,7 +19,7 @@ const pacman = () => {
     let pointUser = user.point;
     pointUser += value;
     axios
-      .put("/game/" + id + `?point=${pointUser}`)
+      .put(`/game/${2}/${id}?point=${pointUser}&title=PACMAN`)
       .then((response) => alert("You win!"))
       .catch((err) => console.log(err));
     setLoading(true);
@@ -40,8 +40,6 @@ const pacman = () => {
       .catch((err) => console.log(err));
   }, [router.isReady, id]);
 
-  useEffect(() => {}, []);
-
   return (
     <section className="flex justify-center bg-[url('/asset/bg-pacman.png')] min-h-screen bg-cover bg-center">
       <div className="flex flex-col items-center text-center">
@@ -51,44 +49,21 @@ const pacman = () => {
           className="relative w-[150px] mt-[70px] inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-white rounded-full shadow-md group"
         >
           <span className="absolute inset-0 flex items-center justify-center w-full h-full text-black duration-300 -translate-x-full bg-yellow-300 group-hover:translate-x-0 ease">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
             </svg>
           </span>
-          <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
-            Generate Score
-          </span>
+          <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Generate Score</span>
           <span className="relative invisible">Button Text</span>
         </button>
         {loading ? (
           <div className="mt-[50px] mr-[30px]">
-            <Loader
-              color={"#ffff32"}
-              loading={loading}
-              size={30}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
+            <Loader color={"#ffff32"} loading={loading} size={30} aria-label="Loading Spinner" data-testid="loader" />
             <p className="text-white mt-[60px] ml-[35px]">Loading...</p>
           </div>
         ) : (
           <div>
-            <h2
-              className={`text-white ${hidden} font-montserrat text-base mt-[40px]`}
-            >
-              Your Score
-            </h2>
+            <h2 className={`text-white ${hidden} font-montserrat text-base mt-[40px]`}>Your Score</h2>
             <p className="text-white text-[60px]  font-pressstart">{point}</p>
           </div>
         )}
--- pages/profile/[profileId].js
+++ pages/profile/[profileId].js
@@ -39,19 +39,13 @@ const Profile = () => {
             <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
             <span className="relative">Home</span>
           </span>
-          <span
-            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-            data-rounded="rounded-lg"
-          ></span>
+          <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
         </div>
       </a>
       <section className=" min-h-screen flex flex-col justify-center bg-[url('/asset/bg-profile.png')] bg-cover">
         <div className="text-center font-bold text-lg mt-10">Profile</div>
         <div className="w-[300px] h-[720px] mx-auto flex flex-col md:w-[600px] ">
-          <div
-            data-id="0"
-            className=" w-[300px] h-[290px] md:w-[600px] md:h-[300px] mt-5 relative inline-block px-4 py-2 font-medium group"
-          >
+          <div data-id="0" className=" w-[300px] h-[290px] md:w-[600px] md:h-[300px] mt-5 relative inline-block px-4 py-2 font-medium group">
             <span className="absolute inset-0 w-full h-full   translate-x-1 translate-y-1 bg-black "></span>
             <span className="absolute inset-0 w-full h-full bg-slate-600 border-2 border-black "></span>
             <span className="relative text-white">
@@ -67,9 +61,7 @@ const Profile = () => {
             <button className="absolute inline-block px-2 py-1 ml-[105px] md:ml-[255px] mt-[-70px] group  ">
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
               <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-              <span className="relative text-black text-sm md:text-md group-hover:text-white">
-                Edit Profile
-              </span>
+              <span className="relative text-black text-sm md:text-md group-hover:text-white">Edit Profile</span>
             </button>
           </a>
 
@@ -88,7 +80,7 @@ const Profile = () => {
                       Date
                     </th>
                     <th scope="col" className="py-3 px-6">
-                      Point
+                      Game
                     </th>
                   </tr>
                 </thead>
@@ -96,18 +88,12 @@ const Profile = () => {
                   {history &&
                     history.map((result, i) => {
                       return (
-                        <tr
-                          key={i}
-                          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                        >
-                          <th
-                            scope="row"
-                            className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                          >
+                        <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                          <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {user.full_name}
                           </th>
                           <td className="py-4 px-6">{result.gameplay}</td>
-                          <td className="py-4 px-6">{user.point}</td>
+                          <td className="py-4 px-6">{result.game_title}</td>
                         </tr>
                       );
                     })}
