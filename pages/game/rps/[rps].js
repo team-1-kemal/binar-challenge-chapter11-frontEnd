631f5e7baf61fb6d368b8818983efa07c1ca4253
madedev <nmaderama@gmail.com>
2022-11-28 20:55:41 +0700

    Revamp Animation & Add Game

--- pages/game/rps/[rps].js
+++ pages/game/rps/[rps].js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "../../api/axios";
-// import "./Game.css";
+import Head from "next/head";
+import { motion, useAnimation } from "framer-motion";
 
 const Game = () => {
   const [userChoice, setUserChoice] = useState("rock");
@@ -9,20 +10,23 @@ const Game = () => {
   const [userScore, setUserScore] = useState(0);
   const [comScore, setComScore] = useState(0);
   const [result, setResult] = useState("Lets see who wins");
+  const [played, setPlayed] = useState();
   const [gameOver, setGameOver] = useState(false);
   const [btnDisabled, setBtnDisabled] = useState(false);
   const [user, setUser] = useState({});
   const [id, setId] = useState("");
   const [token, setToken] = useState("");
+  const [confetti, setConfetti] = useState("hidden");
+  const control = useAnimation();
 
   const choice = ["rock", "paper", "scissors"];
   const router = useRouter();
   const { userId } = router.query;
-  // const navigate = useNavigate();
 
   const handleOnClick = (choice) => {
     setUserChoice(choice);
     generateComChoice();
+    setPlayed(localStorage.setItem("played1", true));
   };
 
   const generateComChoice = () => {
@@ -41,43 +45,74 @@ const Game = () => {
     axios
       .get("/game/" + id, { headers: { Authorization: token } })
       .then((user) => setUser(user.data.data))
-      .catch((err) => router.push("/login"));
+      .catch((err) => console.log(err));
   }, [router.isReady, id]);
 
   useEffect(() => {
     const comboMoves = userChoice + comChoice;
     if (userScore <= 2 && comScore <= 2) {
-      if (comboMoves === "rockscissors" || comboMoves === "paperrock" || comboMoves === "scissorspaper") {
+      if (
+        comboMoves === "rockscissors" ||
+        comboMoves === "paperrock" ||
+        comboMoves === "scissorspaper"
+      ) {
         const updatedUserScore = userScore + 1;
         setUserScore(updatedUserScore);
         if (updatedUserScore === 3) {
           setGameOver(true);
           setResult("Win!");
+          setConfetti("");
           setBtnDisabled(true);
+          control.start({
+            scale: [0, 2, 1.5],
+            transition: { duration: 1, type: "spring" },
+          });
           let pointUser = user.point;
           pointUser += 100;
           axios
             .put(`/game/${1}/${id}?point=${pointUser}&title=ROCK PAPER SCISSOR`)
             .then((response) => alert("You win!"))
-            .catch((err) => router.push("/login"));
+            .catch((err) => console.log(err));
         }
       }
-      if (comboMoves === "paperscissors" || comboMoves === "scissorsrock" || comboMoves === "rockpaper") {
+      if (
+        comboMoves === "paperscissors" ||
+        comboMoves === "scissorsrock" ||
+        comboMoves === "rockpaper"
+      ) {
         const updatedComputerScore = comScore + 1;
         setComScore(updatedComputerScore);
         if (updatedComputerScore === 3) {
           setGameOver(true);
           setResult("Lose!");
+          control.start({
+            scale: [0, 2, 1.5],
+            transition: { duration: 1, type: "spring" },
+          });
           setBtnDisabled(true);
         }
       }
-      if (comboMoves === "rockrock" || comboMoves === "paperpaper" || comboMoves === "scissorsscissors") {
+      if (
+        comboMoves === "rockrock" ||
+        comboMoves === "paperpaper" ||
+        comboMoves === "scissorsscissors"
+      ) {
       }
     }
   }, [userChoice, comChoice]);
 
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+    >
+      <Head>
+        <title>Game Nation - Rock Paper Scissors!</title>
+        <meta name="description" content="Rock Paper Scissors!" />
+      </Head>
+
       <a href="/dashboard">
         <div className="absolute inline-block text-sm group mt-5 ml-5">
           <span className="relative z-10 block px-1 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
@@ -85,50 +120,99 @@ const Game = () => {
             <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
             <span className="relative ">Home</span>
           </span>
-          <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
         </div>
       </a>
       <section className="flex bg-[url('/asset/bg-game.png')] flex-col items-center min-h-screen px-[1rem] py-[0.5rem] bg-cover bg-no-repeat bg-center">
-        <img src="/asset/logo-game.png" className="w-[100px] md:w-[120px]" alt="logo" />
+        <img
+          src="/asset/confetti.gif"
+          className={`${confetti} absolute min-h-screen`}
+        />
+        <img
+          src="/asset/logo-game.png"
+          className="w-[100px] md:w-[120px]"
+          alt="logo"
+        />
         <div className="game_score flex mt-[50px] gap-[70px] md:gap-[350px] lg:gap-[400px] md:mt-[0px]">
           <div>
-            <h1 className="text-center font-montserrat text-white text-md md:text-xl text-user-point">User Points</h1>
-            <h5 className="text-center font-pressstart text-white text-xl md:text-3xl user-point">{userScore}</h5>
+            <h1 className="text-center font-montserrat text-white text-md md:text-xl text-user-point">
+              User Points
+            </h1>
+            <h5 className="text-center font-pressstart text-white text-xl md:text-3xl user-point">
+              {userScore}
+            </h5>
           </div>
           <div>
-            <h1 className="text-center font-montserrat text-white text-md md:text-xl text-user-point">Com Points</h1>
-            <h5 className="text-center font-pressstart text-white text-xl md:text-3xl user-point">{comScore}</h5>
+            <h1 className="text-center font-montserrat text-white text-md md:text-xl text-user-point">
+              Com Points
+            </h1>
+            <h5 className="text-center font-pressstart text-white text-xl md:text-3xl user-point">
+              {comScore}
+            </h5>
           </div>
         </div>
         <div className="flex flex-col items-center mt-[20px]">
           <div className="flex gap-[20px] md:gap-[200px] lg:gap-[250px]">
             <div className="choice-user transform -scale-x-100">
-              <img className="user-hand w-[130px] md:w-[250px]" alt="choice user" src={`/asset/${userChoice}.png`} />
+              <motion.img
+                className="user-hand w-[130px] md:w-[250px]"
+                alt="choice user"
+                src={`/asset/${userChoice}.png`}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ duration: 0.75, type: "spring" }}
+              />
             </div>
             <div className="choice-com">
-              <img className="com-hand w-[130px] md:w-[250px]" alt="choice com" src={`/asset/${comChoice}.png`} />
+              <motion.img
+                className="com-hand w-[130px] md:w-[250px]"
+                alt="choice com"
+                src={`/asset/${comChoice}.png`}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}
+                transition={{ duration: 0.75, type: "spring" }}
+              />
             </div>
           </div>
 
           <div className="game-btn-comp ml-4 mt-[100px] md:mt-[40px]">
             {choice.map((choice, index) => (
-              <button className="button mr-5 w-[70px] md:w-[120px] relative inline-block px-1 py-1 md:py-2 text-sm md:text-lg group" key={index} onClick={() => handleOnClick(choice)} disabled={btnDisabled}>
+              <button
+                className="button mr-5 w-[70px] md:w-[120px] relative inline-block px-1 py-1 md:py-2 text-sm md:text-lg group"
+                key={index}
+                onClick={() => handleOnClick(choice)}
+                disabled={btnDisabled}
+              >
                 <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                 <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                <span className="relative text-black group-hover:text-white">{choice}</span>
+                <span className="relative text-black group-hover:text-white">
+                  {choice}
+                </span>
               </button>
             ))}
           </div>
 
-          <div className="game-result mt-[-90px] md:mt-[-230px]">
+          <motion.div
+            animate={control}
+            className="game-result mt-[-90px] md:mt-[-230px]"
+          >
             {/* <h1>Turn Result: {turnResult}</h1> */}
             {gameOver && (
               <div>
-                <h1 className="text-center font-pressstart text-sm md:text-2xl text-result mt-[-20px]">You</h1>
-                <h1 className="text-result font-pressstart text-lg md:text-4xl text-center">{result}</h1>
+                <h1 className="text-center font-pressstart text-sm md:text-2xl text-result mt-[-20px]">
+                  You
+                </h1>
+                <h1 className="text-result font-pressstart text-lg md:text-4xl text-center">
+                  {result}
+                </h1>
               </div>
             )}
-          </div>
+          </motion.div>
 
           <div className="btn-gameover-comp mt-[130px] md:mt-[220px]">
             {gameOver && (
@@ -137,18 +221,33 @@ const Game = () => {
                 onClick={() => reset()}
               >
                 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
                   </svg>
                 </span>
-                <span className="absolute flex items-center justify-center w-full text-sm h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Restart Game</span>
-                <span className="relative font-montserrat invisible">Restart Game?</span>
+                <span className="absolute flex items-center justify-center w-full text-sm h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+                  Restart Game
+                </span>
+                <span className="relative font-montserrat invisible">
+                  Restart Game?
+                </span>
               </button>
             )}
           </div>
         </div>
       </section>
-    </div>
+    </motion.div>
   );
 };
 
--- pages/profile/[profileId].js
+++ pages/profile/[profileId].js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
+import Loader from "react-spinners/HashLoader";
 import axios from "../api/axios";
+import Head from "next/head";
 
 const Profile = () => {
   const router = useRouter();
   const { userId } = router.query;
-
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({});
   const [token, setToken] = useState("");
   const [id, setId] = useState("");
@@ -17,6 +18,10 @@ const Profile = () => {
     const getId = localStorage.getItem("id");
     setId(getId);
     setToken(getToken);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
   }, [userId]);
 
   useEffect(() => {
@@ -32,6 +37,10 @@ const Profile = () => {
 
   return (
     <>
+      <Head>
+        <title>Game Nation - {user.full_name}</title>
+        <meta name="description" content="Profile Page" />
+      </Head>
       <a href="/dashboard">
         <div className="absolute inline-block text-sm group mt-5 ml-5">
           <span className="relative z-10 block px-1 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
@@ -39,69 +48,100 @@ const Profile = () => {
             <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
             <span className="relative">Home</span>
           </span>
-          <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
         </div>
       </a>
       <section className=" min-h-screen flex flex-col justify-center bg-[url('/asset/bg-profile.png')] bg-cover">
-        <div className="text-center font-bold text-lg mt-10">Profile</div>
-        <div className="w-[300px] h-[720px] mx-auto flex flex-col md:w-[600px] ">
-          <div data-id="0" className=" w-[300px] h-[290px] md:w-[600px] md:h-[300px] mt-5 relative inline-block px-4 py-2 font-medium group">
-            <span className="absolute inset-0 w-full h-full   translate-x-1 translate-y-1 bg-black "></span>
-            <span className="absolute inset-0 w-full h-full bg-slate-600 border-2 border-black "></span>
-            <span className="relative text-white">
-              <div className="text-sm md:text-lg mx-5 md:mx-20 mt-[30px] ">
-                <p className=" font-medium">Full Name: {user.full_name}</p>
-                <p className=" font-medium mt-4">Email: {user.email}</p>
-                <p className=" font-medium mt-4">City: {user.city}</p>
-                <p className=" font-medium mt-4">DoB: {user.dob}</p>
+        {loading ? (
+          <div className="flex flex-col items-center ml-[-10px] mt-[-40px]">
+            <Loader
+              color={"#0b1c0f"}
+              loading={loading}
+              size={100}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            <p className="text-black font-montserrat font-bold ml-[10px] text-2xl mt-[30px]">
+              Loading...
+            </p>
+          </div>
+        ) : (
+          <div>
+            <div className="text-center font-bold text-lg mt-10">Profile</div>
+            <div className="w-[300px] h-[720px] mx-auto flex flex-col md:w-[600px] ">
+              <div
+                data-id="0"
+                className=" w-[300px] h-[290px] md:w-[600px] md:h-[300px] mt-5 relative inline-block px-4 py-2 font-medium group"
+              >
+                <span className="absolute inset-0 w-full h-full   translate-x-1 translate-y-1 bg-black "></span>
+                <span className="absolute inset-0 w-full h-full bg-slate-600 border-2 border-black "></span>
+                <span className="relative text-white">
+                  <div className="text-sm md:text-lg mx-5 md:mx-20 mt-[30px] ">
+                    <p className=" font-medium">Full Name: {user.full_name}</p>
+                    <p className=" font-medium mt-4">Email: {user.email}</p>
+                    <p className=" font-medium mt-4">City: {user.city}</p>
+                    <p className=" font-medium mt-4">DoB: {user.dob}</p>
+                  </div>
+                </span>
               </div>
-            </span>
-          </div>
-          <a href={"/edit/" + id}>
-            <button className="absolute inline-block px-2 py-1 ml-[105px] md:ml-[255px] mt-[-70px] group  ">
-              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-              <span className="relative text-black text-sm md:text-md group-hover:text-white">Edit Profile</span>
-            </button>
-          </a>
+              <a href={"/edit/" + id}>
+                <button className="absolute inline-block px-2 py-1 ml-[105px] md:ml-[255px] mt-[-70px] group  ">
+                  <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                  <span className="relative text-black text-sm md:text-md group-hover:text-white">
+                    Edit Profile
+                  </span>
+                </button>
+              </a>
 
-          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <h3 className="text-center text-xl mt-2">Game History</h3>
-            {!history ? (
-              <h3 className="text-center">No Record</h3>
-            ) : (
-              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-3">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 text-center">
-                  <tr>
-                    <th scope="col" className="py-3 px-6">
-                      Name
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Date
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Game
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="text-center">
-                  {history &&
-                    history.map((result, i) => {
-                      return (
-                        <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                          <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {user.full_name}
-                          </th>
-                          <td className="py-4 px-6">{result.gameplay}</td>
-                          <td className="py-4 px-6">{result.game_title}</td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            )}
+              <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                <h3 className="text-center text-xl mt-2">Game History</h3>
+                {!history ? (
+                  <h3 className="text-center">No Record</h3>
+                ) : (
+                  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-3">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 text-center">
+                      <tr>
+                        <th scope="col" className="py-3 px-6">
+                          Name
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                          Date
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                          Game
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-center">
+                      {history &&
+                        history.map((result, i) => {
+                          return (
+                            <tr
+                              key={i}
+                              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                            >
+                              <th
+                                scope="row"
+                                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                              >
+                                {user.full_name}
+                              </th>
+                              <td className="py-4 px-6">{result.gameplay}</td>
+                              <td className="py-4 px-6">{result.game_title}</td>
+                            </tr>
+                          );
+                        })}
+                    </tbody>
+                  </table>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
+        )}
       </section>
     </>
   );
